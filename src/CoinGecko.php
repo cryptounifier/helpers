51552286fd65b566ab9efbdfc071502ac3854204
miguilim <35383529+srdante@users.noreply.github.com>
2023-08-15 18:17:30 -0300

    Move cryptocurrency to helpers

---
+++ src/CoinGecko.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace CryptoUnifier\Helpers;
+
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Storage;
+
+use Illuminate\Http\Client\RequestException;
+
+class CoinGecko
+{
+    /**
+     * Information cache ttl.
+     */
+    public const CACHE_TTL = 60 * 60 * 1;
+
+    /**
+     * CoinGecko API base URL.
+     */
+    public const API_URL = 'https://api.coingecko.com/api/v3';
+
+
+    /**
+     * Flush cache asset information (If available).
+     */
+    public static function flushAssetInformation(string $apiId): bool
+    {
+        return Cache::forget(self::generateCacheHash($apiId));
+    }
+
+    /**
+     * Get asset information (From cache if available).
+     */
+    public static function getAssetInformation(string $apiId, bool $forceRefresh = false): ?object
+    {
+        $cache = Cache::get(self::generateCacheHash($apiId));
+
+        // Cache not available || (Force refresh || Invalid cache || Cache expired)
+        if (! is_array($cache)) {
+            $result = self::makeApiRequest($apiId);
+
+            self::saveAssetInformationOnCache(
+                apiId: $apiId,
+                result: $result
+            );
+        } elseif ($forceRefresh || ! is_object($cache[0]) || $cache[1] < time()) {
+            $response = self::makeApiRequest($apiId);
+
+            // Validate response. If invalid, re-save the cache
+            $result = (is_object($response))
+                ? $response
+                : $cache[0];
+
+            self::saveAssetInformationOnCache(
+                apiId: $apiId,
+                result: $result,
+            );
+        } else {
+            $result = $cache[0];
+        }
+
+        return $result;
+    }
+
+    /**
+     * Make API request.
+     */
+    protected static function makeApiRequest(string $apiId): ?object
+    {
+        try {
+            $result = Http::retry(3)->get(self::API_URL . "/coins/{$apiId}")->json();
+        } catch (RequestException $e) {
+            self::addErrorLog($apiId, $e->getMessage());
+
+            return null;
+        }
+
+        if (isset($result['status']['error_message']) || ! isset($result['market_data'])) {
+            self::addErrorLog($apiId, $result['status']['error_message'] ?? 'Unknown error and market data not available');
+
+            return null;
+        }
+
+        return (object) $result['market_data'];
+    }
+
+    /**
+     * Generate cache hash based on $apiId.
+     */
+    protected static function generateCacheHash(string $apiId): string
+    {
+        return md5("{$apiId}.asset-information.coingecko");
+    }
+
+    /**
+     * Save/update asset information on cache.
+     */
+    protected static function saveAssetInformationOnCache(string $apiId, ?object $result): bool
+    {
+        return Cache::forever(
+            key: self::generateCacheHash($apiId),
+            value: [$result, time() + self::CACHE_TTL]
+        );
+    }
+
+    /**
+     * Append error log to storage.
+     */
+    protected static function addErrorLog(string $apiId, string $message): bool
+    {
+        return Storage::append(
+            path: 'coingecko-error.log',
+            data: '[' . now()->toDateTimeString() . '] API ID: ' . $apiId . ' - ' . $message . '.'
+        );
+    }
+}
---
+++ src/Cryptocurrency.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace CryptoUnifier\Helpers;
+
+use CryptoUnifier\Helpers\Base\NonDatabaseModel;
+
+use InvalidArgumentException;
+
+use Illuminate\Support\Str;
+use Illuminate\Support\Collection;
+
+abstract class Cryptocurrency extends NonDatabaseModel
+{
+    /**
+     * Fiat currency for conversion
+     *
+     * @var string
+     */
+    protected $currency = 'usd';
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = [];
+
+
+    /**
+     * Get cryptocurrency list information in a collection of instances.
+     *
+     * @return Collection<int, static>
+     */
+    final public static function all(): Collection
+    {
+        return collect(config('custom.cryptocurrencies'))->map(
+            fn ($attributes, $symbol) => new static(array_merge(['symbol' => $symbol], $attributes))
+        )->sortKeys()->values();
+    }
+
+    /**
+     * Get cryptocurrency instance.
+     */
+    final public static function find(string $key): static
+    {
+        $attributes = config("custom.cryptocurrencies.{$key}");
+
+        if ($attributes === null || empty(array_filter($attributes))) {
+            throw new InvalidArgumentException("Cryptocurrency symbol [{$key}] is not supported.");
+        }
+
+        return new static(array_merge(['symbol' => $key], $attributes));
+    }
+
+    /**
+     * Set currency value.
+     */
+    final public function setCurrency(string $value): void
+    {
+        $this->currency = Str::lower($value);
+    }
+
+    /**
+     * Flush CoinGecko cache data.
+     */
+    final public function flushCoingeckoData(): bool
+    {
+        return CoinGecko::flushAssetInformation($this->coingecko_id);
+    }
+
+    /**
+     * Get CoinGecko attribute.
+     */
+    final public function getCoingeckoAttribute(): ?object
+    {
+        return CoinGecko::getAssetInformation($this->coingecko_id);
+    }
+
+    /**
+     * Get CoinGecko Id attribute.
+     */
+    final public function getCoingeckoIdAttribute(): string
+    {
+        return $this->getAttributeFromArray('cg_id') ?? $this->name;
+    }
+
+    /**
+     * Get formatted name attribute.
+     */
+    final public function getFormattedNameAttribute(): string
+    {
+        return Str::headline($this->name);
+    }
+
+    /**
+     * Get price attribute.
+     */
+    final public function getPriceAttribute(): float
+    {
+        return $this->coingecko->current_price[$this->currency] ?? 0;
+    }
+
+    /**
+     * Get market cap attribute.
+     */
+    final public function getMarketCapAttribute(): float
+    {
+        return $this->coingecko->market_cap[$this->currency] ?? 0;
+    }
+
+    /**
+     * Get percentage 30 days variation attribute.
+     */
+    final public function getPercentage30dVariationAttribute(): float
+    {
+        return $this->coingecko->price_change_percentage_30d ?? 0;
+    }
+}

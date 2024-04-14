1b4cb1bc4bf4c25ce02c8b514b7fdb8ccabb395b
miguilim <35383529+srdante@users.noreply.github.com>
2024-04-14 14:15:23 +0800

    Fix IpAddressFactory not working

--- src/IpAddressFactory.php
+++ src/IpAddressFactory.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class IpAddressFactory extends Factory
 {
     /**
+     * The name of the factory's corresponding model.
+     *
+     * @var class-string<\Illuminate\Database\Eloquent\Model>
+     */
+    protected $model = IpAddress::class;
+
+    /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
